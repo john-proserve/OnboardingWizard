b1e9fc441fbcd5e38fb1ea2d429e97158eee5e05
Nick Lanners <nick.lanners@zonos.com>
2023-04-27 14:14:01 -0600

    create nav bar

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const NavList = styled.nav`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const NavLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  font-size: 18px;
+  text-decoration: none;
+  color: black;
+  background-color: white;
+  width: 100%;
+`;
+
+export const Navbar = () => (
+  <NavList>
+    <NavLink to="/general">General</NavLink>
+
+    <NavLink to="/billing">Billing</NavLink>
+
+    <NavLink to="/integration">Integration</NavLink>
+
+    <NavLink to="/carrier-account">Carrier Account</NavLink>
+
+    <NavLink to="/warehouse">Warehouse</NavLink>
+
+    <NavLink to="/shipping-zones">Shipping Zones</NavLink>
+
+    <NavLink to="/rules-flat-rates">Rules</NavLink>
+
+    <NavLink to="/landed-cost-settings">
+      Landed Cost Settings
+    </NavLink>
+
+    <NavLink to="/tax-ids">Tax IDs</NavLink>
+
+    <NavLink to="/buffers">Buffers</NavLink>
+
+    <NavLink to="/team">Team</NavLink>
+
+    <NavLink to="/app-enable">Enable App</NavLink>
+  </NavList>
+);
--- frontend/src/pages/layout.jsx
+++ frontend/src/pages/layout.jsx
@@ -1,7 +1,9 @@
 import { Outlet } from 'react-router-dom';
+import { Navbar } from '../components/navbar';
 
 export const Layout = () => (
   <>
+    <Navbar />
     <Outlet />
   </>
 );
